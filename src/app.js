be8468a457d9034936f0e906199156f8ef480073
TecnnoBot03 <simoncarreno2006@gmail.com>
2023-09-26 17:05:16 -0500

    creada la pagina de los titulos de las peliculas populares de la api, insertados en el html

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
+const container = document.getElementById("container")
 const cargarPeliculas = async() => {
 
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/750?api_key=9d85cf1cbc7c82147b1bfbefb5f55e8c`)
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9d85cf1cbc7c82147b1bfbefb5f55e8c`)
 
             console.log(response)
 
             if (response.status === 200) {
                 const data = await response.json()
 
-                console.log(data)
+                let peliculas = ""
+
+                data.results.forEach(pelicula => {
+                    peliculas = peliculas + `<h1>${pelicula.title}</h1>`
+                })
+
+                container.innerHTML = peliculas
             }
     } catch(error) {
         console.log(error)
