de7418e462e3c7e86750d98cbe3ffd33094c05c8
TecnnoBot03 <simoncarreno2006@gmail.com>
2023-09-28 08:51:36 -0500

    añadida la parte de la imagen de las peliculas, creada la etiqueta main en el html, creado el primer archivo de css para las peliculas

--- src/app.js
+++ src/app.js
@@ -5,17 +5,19 @@ const cargarPeliculas = async() => {
         const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9d85cf1cbc7c82147b1bfbefb5f55e8c`)
 
             console.log(response)
+            
 
             if (response.status === 200) {
                 const data = await response.json()
-
+                console.log(data)
                 let peliculas = ""
 
                 data.results.forEach(pelicula => {
-                    peliculas = peliculas + `<h1>${pelicula.title}</h1>`
+                    peliculas = peliculas + `<div class="movieBox"><img class="movieImg" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"><h1 class="movieTilte">${pelicula.title}</h1></div>`
                 })
 
                 container.innerHTML = peliculas
+
             }
     } catch(error) {
         console.log(error)
